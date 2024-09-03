77b72c42368db8296573f22b4f9b04848967084b
darwish74 <mohammeddarwish66609@gmail.com>
2024-09-03 12:13:40 +0300

    first commit

---
+++ Specifiers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Specifiers
+{                        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int X = 10;
+            //int Y = 20;
+            //Console.WriteLine($"Equation: {X} + {Y} = {X + Y:C}");
+            double percentage = 0.75;
+            Console.WriteLine($"Percentage is : {percentage:P}");
+
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pricePerSmallRoom=25;
+            int pricePerLargeRoom=35;
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.Write("Number of small carpets: ");
+            int numberOfSmallCarpets=int.Parse(Console.ReadLine());
+            Console.Write("Number of large carpets: ");
+            int numberOfLargeCarpets=int.Parse(Console.ReadLine());
+            Console.WriteLine($"Price per Small room: ${pricePerSmallRoom}");
+            Console.WriteLine($"Price per large room: ${pricePerLargeRoom}");
+            int cost=(numberOfSmallCarpets * pricePerSmallRoom)+ (numberOfLargeCarpets* pricePerLargeRoom);
+            Console.WriteLine($"Cost: ${cost}");
+            Console.WriteLine($"Tax: ${cost * 0.06}");
+            Console.WriteLine("================================");
+            Console.WriteLine($"Total estimate: ${cost * 1.06}");
+            Console.WriteLine($"This estimate is valid for 30 days");
+        }
+    }
+}
